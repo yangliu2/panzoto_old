6963fa5cf876be8321eeed109ca972b5778cd44f
yangliu2 <yangliu3456@gmail.com>
2016-11-08 21:44:39 -0500

    add time report

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from user import User
 import mysql
 import util
+from datetime import datetime
 
 def get_name(string):
 	'''get the first and last name'''
@@ -24,27 +25,45 @@ def get_name(string):
 	return first.title(), last.title()
 
 def menu():
+
+	# get the logo from util.py
 	util.logo()
+
+	# prompt for a name
 	response = raw_input("Please enter your name: ")
 
 	first, last = get_name(response)
 
+	# get user info from user.py
 	user = User(first, last)
 
 	return user
 
+def what(question):
+	'''process questions start with 'what'. '''
+
+	if 'what time is it' in question:
+		return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
 def process_response(user_response):
-	
+	''' 
+	the general processing function for the user response, will separate processing functions into difference categories 
+	'''
+
 	user_response = user_response.lower()
 
 	if user_response == 'exit':
 		AI_response = 'See you!'
+	elif user_response.startswith('what'):
+		AI_response = what(user_response)
 	else:
 		AI_response = 'i cannot understand :('
 
 	return AI_response
 
 def question_loop(user):
+	''' question loop that will quit with 'exit' keyword'''
+
 	user_response = ""
 	while user_response != 'exit':
 		user_response = raw_input("Panzoto > ")
